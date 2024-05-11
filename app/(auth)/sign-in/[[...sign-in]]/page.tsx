b61937189b9fd016a085c59e338ab29024a991ab
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-11 03:18:33 +0000

    styles added in auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,34 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 const SignInPage = () => {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or create account to get back to your dashboard.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src="/logo.svg" alt="Finance logo" height={100} width={100} />
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,34 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 const SignUpPage = () => {
-  return <SignUp path="/sign-up" />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or create account to get back to your dashboard.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src="/logo.svg" alt="Finance logo" height={100} width={100} />
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
