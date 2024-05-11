f17f2df3141a21598bbd578390241f48d9119cdb
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-11 05:16:33 +0000

    mobile navbar implemented

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -11,8 +11,16 @@ type NavButtonProps = {
 
 export const NavButton = ({ href, label, isActive }: NavButtonProps) => {
   return (
-    <Button size="sm" variant="outline" className={cn("w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-offset-0 text-white focus:bg-white/30 transition", isActive ? "bg-white/10 text-white" : "bg-transparent")} asChild>
-        <Link href={href}>{label}</Link>
+    <Button
+      size="sm"
+      variant="outline"
+      className={cn(
+        "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-offset-0 text-white focus:bg-white/30 transition",
+        isActive ? "bg-white/10 text-white" : "bg-transparent"
+      )}
+      asChild
+    >
+      <Link href={href}>{label}</Link>
     </Button>
-  )
+  );
 };
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { Menu } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useMedia } from "react-use";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import { NavButton } from "./nav-button";
 
@@ -28,7 +34,47 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
   const pathname = usePathname();
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onClick = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left" className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                key={route.href}
+                variant={route.href === pathname ? "secondary" : "ghost"}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
 
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
