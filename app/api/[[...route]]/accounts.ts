90d2c0f40d13ad910ae0dc6984d658f15d0577b7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-11 07:18:18 +0000

    inser account endpoint added

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,26 +1,58 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 
-const app = new Hono().get("/", clerkMiddleware(), async (ctx) => {
-  const auth = getAuth(ctx);
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (ctx) => {
+    const auth = getAuth(ctx);
 
-  if (!auth?.userId) {
-    return ctx.json({ error: "Unauthorized." }, 401);
-  }
+    if (!auth?.userId) {
+      return ctx.json({ error: "Unauthorized." }, 401);
+    }
 
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId));
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-  return ctx.json({ data });
-});
+    return ctx.json({ data });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertAccountSchema.pick({
+        name: true,
+      })
+    ),
+    async (ctx) => {
+      const auth = getAuth(ctx);
+      const values = ctx.req.valid("json");
+
+      if (!auth?.userId) {
+        return ctx.json({ error: "Unauthorized." }, 401);
+      }
+
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+
+      return ctx.json({ data });
+    }
+  );
 
 export default app;
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,10 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const insertAccountSchema = createInsertSchema(accounts);
