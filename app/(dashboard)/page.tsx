fe62fe2cbb7ba4aebd43339dad5ec12b3f5fa06a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-11 02:59:19 +0000

    file and folder structure

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Button variant="destructive" size="lg">
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default DashboardPage;
