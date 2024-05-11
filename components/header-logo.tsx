f7810c1ba201b52ba2c73d820542dfb8aad18601
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-11 05:07:14 +0000

    desktop navbar implemented

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -6,6 +6,7 @@ export const HeaderLogo = () => {
     <Link href="/">
       <div className="items-center hidden lg:flex">
         <Image src="/logo.svg" alt="Finance logo" height={28} width={28} />
+
         <p className="font-semibold text-white text-2xl ml-2.5">Finance</p>
       </div>
     </Link>
--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 type NavButtonProps = {
   href: string;
   label: string;
@@ -5,5 +10,9 @@ type NavButtonProps = {
 };
 
 export const NavButton = ({ href, label, isActive }: NavButtonProps) => {
-  return <div className="">NavButton</div>;
+  return (
+    <Button size="sm" variant="outline" className={cn("w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-offset-0 text-white focus:bg-white/30 transition", isActive ? "bg-white/10 text-white" : "bg-transparent")} asChild>
+        <Link href={href}>{label}</Link>
+    </Button>
+  )
 };
