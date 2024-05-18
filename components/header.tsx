6f1b195cae5b87e69c7a599131c1fcdfe0083d82
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-18 16:21:15 +0000

    source code link added

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,9 @@
 import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { links } from "@/config";
 
 import { Filters } from "./filters";
 import { HeaderLogo } from "./header-logo";
@@ -8,7 +12,7 @@ import { WelcomeMsg } from "./welcome-msg";
 
 export const Header = () => {
   return (
-    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 lg:pb-36">
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 lg:pb-32">
       <div className="mx-auto max-w-screen-2xl">
         <div className="mb-14 flex w-full items-center justify-between">
           <div className="flex items-center lg:gap-x-16">
@@ -16,13 +20,24 @@ export const Header = () => {
             <Navigation />
           </div>
 
-          <ClerkLoaded>
-            <UserButton afterSignOutUrl="/" />
-          </ClerkLoaded>
+          <div className="flex items-center gap-x-2">
+            <ClerkLoaded>
+              <UserButton afterSignOutUrl="/" />
+            </ClerkLoaded>
 
-          <ClerkLoading>
-            <Loader2 className="size-8 animate-spin text-slate-400" />
-          </ClerkLoading>
+            <ClerkLoading>
+              <Loader2 className="size-8 animate-spin text-slate-400" />
+            </ClerkLoading>
+
+            <Link
+              href={links.sourceCode}
+              target="_blank"
+              rel="noreferrer noopener"
+              title="Source Code"
+            >
+              <Image src="/github.svg" alt="GitHub" height={24} width={24} />
+            </Link>
+          </div>
         </div>
 
         <WelcomeMsg />
