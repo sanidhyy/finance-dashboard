0af2575bbceb8db9452fd069ef1855f174809016
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-06-14 11:09:10 +0530

    fix: price formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,11 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function convertAmountFromMilliunits(amount: number) {
-  return Math.round(amount / 1000);
+  return Number((amount / 1000).toFixed(2));
 }
 
 export function convertAmountToMilliunits(amount: number) {
-  return Math.round(amount * 1000);
+  return Number((amount * 1000).toFixed(2));
 }
 
 export function formatCurrency(value: number) {
